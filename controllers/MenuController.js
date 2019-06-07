b90b87f474c2d50e97e3e40d75797f7f00cd38ad
Summer Guijarro <summerguijarro@Summers-MacBook-Air.local>
2019-06-07 16:55:29 -0700

    bloc-address-checkpoint4 Complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,8 @@ module.exports = class MenuController {
           message: "Please choose from an option below: ",
           choices: [
             "Add new contact",
+            "View all contacts",
+            "Search for a contact",
             "Today's Date",
             "Exit"
           ]
@@ -25,6 +27,12 @@ module.exports = class MenuController {
          case "Add new contact":
            this.addContact();
            break;
+         case "View all contacts":
+           this.getContacts();
+           break;
+         case "Search for a contact":
+           this.search();
+           break;  
          case "Today's Date":
            this.getDate();
            break;
@@ -74,6 +82,95 @@ module.exports = class MenuController {
 
     remindMe(){
         return "Learning is a life-long pursuit";
+    }
+
+    getContacts(){
+      this.clear();
+
+      this.book.getContacts().then((contacts) => {
+        for (let contact of contacts) {
+          console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`
+          );
+        }
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+    search(){
+      inquirer.prompt(this.book.searchQuestions)
+      .then((target) => {
+       this.book.search(target.name)
+       .then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+         }
+
+        });
+     })
+     .catch((err) => {
+       console.log(err);
+       this.main();
+     });
+    }
+
+    showContact(contact){
+      this._printContact(contact);
+      inquirer.prompt(this.book.showContactQuestions)
+      .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+    }
+
+    _printContact(contact){
+      console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+      );
+    }
+
+    delete(contact){
+      inquirer.prompt(this.book.deleteConfirmQuestions)
+      .then((answer) => {
+        if(answer.confirmation){
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.main();
+      });
     }
  
 }
